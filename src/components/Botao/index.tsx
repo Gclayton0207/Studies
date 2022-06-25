9c307628eef86c29f655fa4c54f610924303d061
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-06-25 11:35:23 -0300

    refactor:instalando css module

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import './style.scss';
+import style from './Botao.module.scss';
 
 const index = () => {
   return (
     <div>
-      <button className="botao">Botão</button>
+      <button className={style.botao}>Botão</button>
     </div>
   )
 }

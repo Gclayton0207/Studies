88e5e0dd1c21f193fae644c39270627490dbcff1
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-06-27 09:50:56 -0300

    refactor:criando componente item da lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import style from './Lista.module.scss';
+import Item from "./Item";
+import style from "./Lista.module.scss";
 
 const index = () => {
   const tarefas = [
@@ -22,10 +23,10 @@ const index = () => {
         <h2>Estudos do dia</h2>
         <ul>
           {tarefas.map((item, index) => (
-            <li key={index} className={style.item}>
-              <h3>{item.tarefa}</h3>
-              <span>{item.tempo}</span>
-            </li>
+            <Item
+            key={index}
+            {...item}
+            />
           ))}
         </ul>
       </aside>
